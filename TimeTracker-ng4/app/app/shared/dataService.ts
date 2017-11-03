1f0f92b963c085231b9b1886971f8fb7c22f1c18
Orest <orest.khrapko@cardworks.com>
2017-11-03 17:40:25 -0400

    Add project files.

---
+++ TimeTracker-ng4/app/app/shared/dataService.ts
@@ -0,0 +1,11 @@
+﻿import {Injectable} from "@angular/core"
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import "rxjs/add/operator/map";
+
+
+@Injectable()
+export class DataService {
+    constructor(private http: HttpClient) {
+
+    }
+}
---
+++ TimeTracker-ng4/app/app/todo.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from "@angular/core"
+
+@Component({
+    selector: "todos",
+    templateUrl:"./todo.component.html"
+})
+export class TodoComponent {
+    
+}
+     
